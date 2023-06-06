fb898d025177a591b0af66c45701670545125734
elgharbi <elgharbiay@gmail.com>
2023-06-05 17:41:02 -0700

    task 10 function in C that checks if a singly linked list has a cycle in it

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * check_cycle - finds a loop in a linked list
+ * @list: list to take in
+ * Return: 1 if loop 0 if no loop
+ */
+
+int check_cycle(listint_t *list)
+{
+	listlint_t *first, *second;
+
+	first = list;
+	second = list;
+	while (first && second && second->next)
+	{
+		first = first->next;
+		second = second->next->next;
+
+		if (first == second)
+			return (1);
+	}
+	return (0);
+}
