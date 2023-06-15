4e9a8d08312a20c7a09d4c406c885b05ddf1eb54
elgharbi <elgharbiay@gmail.com>
2023-06-15 01:09:35 +0100

    miss method items

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        for key, val in a_dictionary:
+        for key, val in a_dictionary.items():
             if val == max(a_dictionary.values()):
                 return key
