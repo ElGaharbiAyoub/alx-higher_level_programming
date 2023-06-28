a9cef870e23b1b9fa5c51f2d96a6aebb202b301c
elgharbi <elgharbiay@gmail.com>
2023-06-28 02:39:51 +0100

    tasks 1  Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    Represents a square object.
+
+    Attributes:
+        _size (int): Size of the square.
+    """
+
+    def __init__(self, size):
+        """
+            Initializes a square with the given size.
+
+            Args:
+                size (int): Size of the square.
+            """
+
+        self.__size = size
