162d2187bf5de05bd1d3178db320a90e114d71e5
elgharbi <elgharbiay@gmail.com>
2023-06-29 01:01:32 +0100

    fix except par

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,7 +1,7 @@
 def safe_print_division(a, b):
     try:
         value = a / b
-    except:
+    except ZeroDivisionError:
         value = None
     finally:
         print("Inside result: {}".format(value))
