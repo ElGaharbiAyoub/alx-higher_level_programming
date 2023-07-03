73f45be36080073c6a2795acd5811f342130e2dd
elgharbi <elgharbiay@gmail.com>
2023-07-03 22:24:55 +0100

    add String representation

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""
+rectagle class
+"""
+
+
+class Rectangle:
+    """Represents a rectangle with a width, height"""
+
+    def __init__(self, width=0, height=0):
+        """Initializes the data."""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """return width value"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """set width value"""
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        """return height value"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """set height value"""
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
+
+    def area(self):
+        """
+        method that returns the rectangle area
+        """
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """
+        method that returns the rectangle perimeter
+        """
+        if self.height == 0 or self.width == 0:
+            return 0
+        return (self.height + self.width) * 2
+
+    def __str__(self):
+        if not self.perimeter():
+            return ""
+
+        pattern = ['#' * self.width for _ in range(self.height)]
+        return '\n'.join(pattern)
