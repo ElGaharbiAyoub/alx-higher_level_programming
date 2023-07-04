a7f57e9785c36c31717de437bbcfd125bf35399d
elgharbi <elgharbiay@gmail.com>
2023-07-05 00:13:44 +0100

    fix pycodestyle

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -8,9 +8,9 @@ def add_integer(a, b=98):
     """
     these comments are very stupid
     """
-    if not isinstance(a,(int,float)):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    
+
     return int(a) + int(b)
