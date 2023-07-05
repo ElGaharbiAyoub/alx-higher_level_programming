23c3d0ae2e260cac4377b6f51eed1e59a8e2a0af
elgharbi <elgharbiay@gmail.com>
2023-07-05 22:59:40 +0100

    add some conditions

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -11,13 +11,14 @@ def matrix_divided(matrix, div):
     function that divides all elements of a matrix
     """
     sizeErr = "Each row of the matrix must have the same size"
+    typeError = "matrix must be a matrix (list of lists) of integers/floats"
     for x in matrix:
         if len(x) != len(matrix[0]):
             raise TypeError(sizeErr)
         for y in x:
-            if not isinstance(y, (int, float)):
-                raise TypeError(
-                    "matrix must be a matrix (list of lists) of integers/floats")
+            if not (isinstance(y, (int, float))) or type(x) is not list \
+                    or type(matrix) is not list or type(y) is not list:
+                raise TypeError(typeError)
     if not (isinstance(div, (int, float))):
         raise TypeError("div must be a number")
     if div == 0:
