20f5cdbe5de3e41096aac6b7c8157556d403849f
elgharbi <elgharbiay@gmail.com>
2023-07-05 01:02:18 +0100

    Print square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""print square module"""
+
+
+def print_square(size):
+    """
+        Function that prints a square with the character "#"
+    """
+    if size is 0:
+        return
+    if not isinstance(size, int):
+        raise TypeError('size must be an integer')
+    if size < 0:
+        raise ValueError('size must be >= 0')
+    print('\n'.join('#' * size for x in range(size)))
