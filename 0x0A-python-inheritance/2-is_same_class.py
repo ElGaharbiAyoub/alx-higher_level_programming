168a080ae85ead2e2708ad3661fb58e35aafafba
elgharbi <elgharbiay@gmail.com>
2023-07-10 23:36:01 +0100

    fix style

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-""" 
-function that returns True if the object is exactly an instance    
+"""
+function that returns True if the object is exactly an instance
 of the specified class ; otherwise False
 """
 
