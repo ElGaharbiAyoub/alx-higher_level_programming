0bad8ebed9ac9de8610896b81cc2b209daa9dd91
elgharbi <elgharbiay@gmail.com>
2023-07-13 00:48:13 +0100

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-"""loads adds saves json to file"""
+"""
+lscript that adds all arguments to a Python list,
+and then save them to a file
+"""
 import json
 from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+
 try:
     jsonList = load_from_json_file("add_item.json")
-except:
+except FileNotFoundError:
     jsonList = []
 
 for i in argv[1:]:
