122976cc14947ac3f4afd07750f4031a1105be1d
elgharbi <elgharbiay@gmail.com>
2023-07-13 01:34:15 +0100

    Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" class Student"""
+
+
+class Student:
+    """heraab"""
+
+    def __init__(self, first_name, last_name, age):
+        """initializes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """return __dict__"""
+        if (isinstance(attrs, list) and
+                all(isinstance(x, str) for x in attrs)):
+            return ({x: y for x, y in self.__dict__.items() if x in attrs})
+        else:
+            return self.__dict__
