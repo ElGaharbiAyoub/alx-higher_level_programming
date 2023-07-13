42fc9c575ad704c530398f0bbbe071a498863319
elgharbi <elgharbiay@gmail.com>
2023-07-13 01:00:42 +0100

    fix script

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,7 +3,6 @@
 lscript that adds all arguments to a Python list,
 and then save them to a file
 """
-import json
 from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
@@ -14,6 +13,5 @@ try:
 except FileNotFoundError:
     jsonList = []
 
-for i in argv[1:]:
-    jsonList.append(i)
+jsonList.extend(argv[1:])
 save_to_json_file(jsonList, "add_item.json")
