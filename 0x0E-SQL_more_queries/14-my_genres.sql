33104553a9f2011c6c5b06fc97bd7c8b48f882fb
elgharbi <elgharbiay@gmail.com>
2023-08-17 02:39:05 +0100

    fix

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,8 +1,6 @@
 -- genre id by show
-SELECT tgg.name FROM tv_genres AS tgg
-JOIN tv_show_genres AS tgs
-ON tgs.genre_id = tgg.id
-JOIN tv_shows AS ts
-ON ts.id = tgs.show_id
-WHERE ts.title='Dexter'
-GROUP BY tgg.name ASC;
+SELECT name FROM tv_genres
+JOIN tv_show_genres ON id=tv_show_genres.genre_id
+JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY name;
--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,8 +1,6 @@
 -- only comedy
-SELECT ts.title FROM tv_shows AS ts
-JOIN tv_show_genres AS tgs
-ON ts.id = tgs.show_id
-JOIN tv_genres AS tgg
-ON tgs.genre_id = tgg.id
-WHERE tgg.name = 'Comedy'
-GROUP BY ts.title ASC;
+SELECT title FROM tv_shows
+JOIN tv_show_genres ON id=tv_show_genres.show_id
+JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY title;
