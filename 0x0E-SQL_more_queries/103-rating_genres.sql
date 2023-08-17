32a1efc7b1c63b44f899c71d466929ac0c63063e
elgharbi <elgharbiay@gmail.com>
2023-08-17 15:29:48 +0100

    fix

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,7 +1,6 @@
 -- This list all tv show ratings
-SELECT tg.name, SUM(tr.rate) AS rating FROM tv_show_genres tgs
-JOIN tv_genres tg
-ON tg.id = tgs.genre_id
-JOIN tv_show_ratings tr
-ON tr.show_id = tgs.show_id
-GROUP BY  tg.name DESC ORDER BY RATING DESC;
+SELECT `name`, SUM(`tv_show_ratings`.`rate`) 'rating' FROM `tv_genres`
+	INNER JOIN `tv_show_genres` ON `tv_genres`.`id` = `tv_show_genres`.`genre_id`
+	INNER JOIN `tv_show_ratings` ON `tv_show_genres`.`show_id` = `tv_show_ratings`.`show_id`
+	GROUP BY `name`
+	ORDER BY `rating` DESC;
