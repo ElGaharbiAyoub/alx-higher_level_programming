93f925a34aee02888dd934c66ef732f7c4119b45
elgharbi <elgharbiay@gmail.com>
2023-08-17 02:08:19 +0100

    fiox

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- cities by states
-SELECT c.id, c.name AS city_name, s.name AS state_name
-FROM cities c, states s
-WHERE c.state_id = s.id
-ORDER BY c.id ASC;
+SELECT cities.id, cities.name, states.name
+FROM cities 
+JOIN states
+ON states.id = cities.state_id;:
