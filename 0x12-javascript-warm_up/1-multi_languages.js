0bab1eb4deaa50879c51e86d54fffff535171f19
elgharbi <elgharbiay@gmail.com>
2023-09-11 19:04:05 +0100

    fix

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (const lan of languages) {
-  console.log(lan);
-}
+
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
