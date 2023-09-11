67ba26850d5ffe18256beac172dab545979571a3
elgharbi <elgharbiay@gmail.com>
2023-09-11 19:27:36 +0100

    fix

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,8 +2,8 @@
 
 const args = process.argv.slice(2);
 
-if (Number(args[0])) {
-  console.log('My number:' + parseInt(args[0]));
+if (args.length === 1 && !isNaN(args[0])) {
+  console.log('My number: ' + parseInt(args[0]));
 } else {
   console.log('Not a number');
 }
