3035cc5ad7391a123042fcaa3ec7d8e016fbbf8b
elgharbi <elgharbiay@gmail.com>
2023-09-11 19:31:43 +0100

    fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,7 @@ const args = process.argv.slice(2).map(Number);
 const sortedArgs = args.sort((a, b) => a - b);
 
 if (args.length <= 1) {
-  console.log(1);
+  console.log(0);
 } else {
   console.log(sortedArgs[sortedArgs.length - 2]);
 }
