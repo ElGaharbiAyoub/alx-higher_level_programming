b623072523436ae29f3e974bf57b2b9d3c0e1c48
elgharbi <elgharbiay@gmail.com>
2023-09-12 23:14:55 +0100

    done

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+  const filterList = list.filter((item) => {
+    return item === searchElement;
+  });
+  return filterList.length;
+};
