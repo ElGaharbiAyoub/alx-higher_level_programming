c5f2627416057b9e0c30fb4ebc7373edeea48478
elgharbi <elgharbiay@gmail.com>
2023-09-13 00:04:26 +0100

    done

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+
+exports.esrever = function (list) {
+  const rev = [];
+  const size = list.length - 1;
+  for (const i in list) {
+    rev[i] = list[size - i];
+  }
+  return rev;
+};
