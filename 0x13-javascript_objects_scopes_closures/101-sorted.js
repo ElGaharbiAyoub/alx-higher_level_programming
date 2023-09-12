e6eb2516165905d4386e4cacf06681dfa31ea2a5
elgharbi <elgharbiay@gmail.com>
2023-09-13 00:53:00 +0100

    done

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+
+const dict = require('./101-data.js').dict;
+
+const newDict = {};
+
+for (const key in dict) {
+  const val = dict[key];
+  if (newDict[val]) {
+    newDict[val].push(key);
+  } else {
+    newDict[val] = [key];
+  }
+}
+console.log(newDict);
