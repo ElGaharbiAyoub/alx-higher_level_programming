7e3e53410169b6c1b59558878753eee4a8eba34b
elgharbi <elgharbiay@gmail.com>
2023-09-12 20:16:29 +0100

    done

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,21 @@
 #!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let line = '';
+    for (let j = 0; j < this.width; j++) {
+      line += 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(line);
+    }
+  }
+}
+
+module.exports = Rectangle;
