e34fb38ff0151cb8475a181c75baf36843ed1f66
elgharbi <elgharbiay@gmail.com>
2023-09-12 22:26:42 +0100

    done

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,21 @@
 #!/usr/bin/node
+const square = require("./5-square");
+class Square extends square {
+  constructor(size) {
+    super(size, size);
+  }
+  charPrint (c){
+    if (typeof c === 'undefined'){
+        c = 'X';
+    }
+    let line = '';
+    for (let j = 0; j < this.width; j++) {
+      line += c;
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(line);
+    }
+
+  }
+}
+module.exports = Square;
