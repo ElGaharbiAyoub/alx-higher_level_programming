4e5a2b67764d036bf5684030de6e8679d19ed041
elgharbi <elgharbiay@gmail.com>
2023-09-16 16:21:01 +0100

    Filter states by user inpu

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+"""gets all states via python
+"""
+
+
+def main(args):
+    """gets all state
+    """
+    db = MySQLdb.connect(
+        host='localhost',
+        user=args[1],
+        passwd=args[2],
+        db=args[3])
+    cur = db.cursor()
+    query = (
+        "SELECT * FROM states "
+        "WHERE name LIKE BINARY '{}%' "
+        "ORDER BY id ASC"
+    ).format(args[4])
+    cur.execute(query)
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+    main(sys.argv)
