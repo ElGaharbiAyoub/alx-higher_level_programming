e6b933a8d04fad11e17d0340ae77d03697d6813e
elgharbi <elgharbiay@gmail.com>
2023-09-16 21:08:30 +0100

    All cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+"""
+takes in the name of a state as an argument
+and lists all cities of that state
+"""
+
+
+def main(args):
+    """ All cities by state
+    """
+    db = MySQLdb.connect(
+        host='localhost',
+        user=args[1],
+        passwd=args[2],
+        db=args[3])
+    cur = db.cursor()
+    query = (
+        "SELECT c.name "
+        "FROM cities c "
+        "JOIN states s ON s.id = c.state_id "
+        "WHERE s.name = %s "
+        "ORDER BY c.id ASC"
+    )
+    cur.execute(query, (args[4],))
+    states = cur.fetchall()
+    print(", ".join(map(lambda x: "%s" % x, states)))
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+    main(sys.argv)
