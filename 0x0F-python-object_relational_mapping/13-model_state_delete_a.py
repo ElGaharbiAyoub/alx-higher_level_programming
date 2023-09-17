c61e333acf2bb36c6759a83fce0c097f78b6cd32
elgharbi <elgharbiay@gmail.com>
2023-09-17 18:16:27 +0100

    Delete states

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""
+get state
+"""
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    delete_states = session.query(State).filter(
+        State.name.like('%a%')).all()
+    for state in delete_states:
+        session.delete(state)
+    session.commit()
+        
+    session.close()
