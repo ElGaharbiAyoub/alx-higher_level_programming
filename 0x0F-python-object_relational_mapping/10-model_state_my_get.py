95c212a06b65c463e66aada6b2f30cf758a75b2b
elgharbi <elgharbiay@gmail.com>
2023-09-17 17:58:13 +0100

    fix

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Start link class to table in database
+"""
+get state
 """
 import sys
 from model_state import Base, State
@@ -15,9 +16,9 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     state_name = sys.argv[4]
-    state = session.query(State).filter(State.name == state_name).first()
+    state = session.query(State).filter(State.name == state_name).order_by(State.id).first()
     try:
-        print("{}".format(state.id))
+        print(state.id)
     except:
         print("Nothing")
 
