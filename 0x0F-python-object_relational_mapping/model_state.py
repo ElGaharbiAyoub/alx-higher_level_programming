859d649e78b491aa7882d8f3771fc5bd9bc3d432
elgharbi <elgharbiay@gmail.com>
2023-09-17 01:05:52 +0100

    done

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
 Base = declarative_base()
+"""class State"""
 
 
 class State(Base):
