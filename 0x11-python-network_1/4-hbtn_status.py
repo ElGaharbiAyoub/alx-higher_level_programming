5ff56e684935645fae99fc62d8e953c8fb461d27
elgharbi <elgharbiay@gmail.com>
2023-09-29 23:54:03 +0100

    What's my status?

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+fetches status from https://alx-intranet.hbtn.io/status
+"""
+import requests
+
+
+if __name__ == "__main__":
+    req = requests.get("https://alx-intranet.hbtn.io/status")
+    content = req.text
+    print("Body response:")
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))
