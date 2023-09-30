35306884c9cba5afa950ddc18247317e88226275
elgharbi <elgharbiay@gmail.com>
2023-09-30 01:06:04 +0100

    POST an email

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""
+sends a request to the URL and
+displays the value of the variable
+X-Request-Id in the response header
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    payload = {'email': sys.argv[2]}
+    res = requests.post(sys.argv[1], data=payload)
+    print(res.text)
