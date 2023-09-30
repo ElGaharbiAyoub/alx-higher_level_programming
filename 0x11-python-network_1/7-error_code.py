0c216a96edab489c42ce4009c616c00cfafdc9cf
elgharbi <elgharbiay@gmail.com>
2023-09-30 01:13:29 +0100

    Error code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""
+sends a request to the URL and
+displays the body of the response.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    res = requests.get(sys.argv[1])
+    code = res.status_code
+    if code >= 400:
+        print("Error code: ".format(code))
+    else:
+        print(res.text)
