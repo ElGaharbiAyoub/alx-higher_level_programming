9810a4908957378b748708de39fa2d687ab54b7a
elgharbi <elgharbiay@gmail.com>
2023-09-30 03:20:50 +0100

    Time for an interview!

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""
+script that takes 2 arguments in order to solve this challenge
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    api_url = "https://api.github.com/repos/{}/{}/commits".format(
+            sys.argv[2], sys.argv[1])
+    res = requests.get(api_url).json()
+    try:
+        for i in range(10):
+            print(
+                "{}: {}".format(
+                    res[i].get("sha"),
+                    res[i].get("commit").get("author").get("name"),
+                )
+            )
+    except IndexError:
+        pass
