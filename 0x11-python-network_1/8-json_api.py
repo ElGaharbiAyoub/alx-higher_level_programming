da4e1d578341cab59631e5632c05a53b73a8f449
elgharbi <elgharbiay@gmail.com>
2023-09-30 01:35:10 +0100

    Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""
+takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    payload = {'q': sys.argv[1] if len(sys.argv) > 1 else ""}
+    url = "http://0.0.0.0:5000/search_user"
+    res = requests.post(url, data=payload)
+    try:
+        jsn = res.json()
+        if jsn:
+            print("[{}] {}".format(jsn.get('id'), jsn.get('name')))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
