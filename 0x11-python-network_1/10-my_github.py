c2af4604e109380d4774c67d6b67128fefa8d9b5
elgharbi <elgharbiay@gmail.com>
2023-09-30 02:46:14 +0100

    My GitHub!

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    api_url = "https://api.github.com/user"
+    res = requests.get(api_url, auth=(sys.argv[1], sys.argv[2]))
+    print(res.json().get("id"))
