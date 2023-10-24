3c96bf52be2096a34a1f9da640c432b445873e17
elgharbi <elgharbiay@gmail.com>
2023-10-25 00:23:42 +0100

    Right order

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+
+const request = require('request');
+const id = process.argv[2];
+
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.error('Error: Unexpected status code', response.statusCode);
+  } else {
+    try {
+      const characters = JSON.parse(body).characters;
+      promises = [];
+      for (const char of characters) {
+        promises.push(
+          new Promise(function (resolve, reject) {
+            request(char, (err, res, bd) => {
+              resolve(JSON.parse(bd).name);
+            });
+          })
+        );
+      }
+      Promise.all(promises).then((a) => {
+        for (const name of a) {
+          console.log(name);
+        }
+      });
+    } catch (parseError) {
+      console.error('Error parsing JSON:', parseError);
+    }
+  }
+});
