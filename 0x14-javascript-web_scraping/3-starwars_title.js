0990b2c9dac9ccfdc32cbba91e2fdf46c8d53eac
elgharbi <elgharbiay@gmail.com>
2023-10-24 22:10:04 +0100

    DONE

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const request = require("request");
+const id = process.argv[2];
+
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error("Error:", error);
+  } else if (response.statusCode !== 200) {
+    console.error("Error: Unexpected status code", response.statusCode);
+  } else {
+    try {
+      const data = JSON.parse(body);
+      console.log(data.title);
+    } catch (parseError) {
+      console.error("Error parsing JSON:", parseError);
+    }
+  }
+});
