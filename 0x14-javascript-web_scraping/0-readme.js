c9ec6739502f93838242be3ac9932eb8273249e6
elgharbi <elgharbiay@gmail.com>
2023-10-24 21:35:56 +0100

    done

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-let file = process.argv[2];
-let fs = require("fs");
+const file = process.argv[2];
+const fs = require('fs');
 
-fs.readFile(file, "utf8", function (err, data = err) {
+fs.readFile(file, 'utf8', function (err, data = err) {
   console.log(data);
 });
