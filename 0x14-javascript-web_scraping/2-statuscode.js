b84316584f97880b5c58520caf24244b02ec97e3
elgharbi <elgharbiay@gmail.com>
2023-10-25 00:35:01 +0100

    fix

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
-const request = require('request');
+const request = require("request");
 const url = process.argv[2];
 
 request(url, function (error, response, body) {
   if (error) {
-    console.error('Error:', error);
+    console.error("Error:", error);
   } else {
-    console.log('code :', response.statusCode);
+    console.log("code:", response.statusCode);
   }
 });
